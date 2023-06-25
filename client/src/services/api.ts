6654a58b4cc6e3e78a0b7db59d65e326748bae6e
guilhermevialle <vialleguilherme@gmail.com>
2023-06-25 10:33:59 -0300

    trying to solve CORS

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -2,4 +2,7 @@ import { io } from 'socket.io-client'
 
 export const socket = io('https://talkonline-app-api.vercel.app', {
   withCredentials: true,
+  extraHeaders: {
+    'Access-Control-Allow-Origin': 'true',
+  },
 })
