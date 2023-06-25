4deaaebc6a8c5acd0c55ee41ab1a23131fe63698
guilhermevialle <vialleguilherme@gmail.com>
2023-06-25 12:00:39 -0300

    CORS must be on Vercel because this fkng problem dont disappear

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -1,5 +1,5 @@
 import { io } from 'socket.io-client'
 
-export const socket = io('http://localhost:9090', {
+export const socket = io('https://talkonline-app-api.vercel.app', {
   withCredentials: true,
 })
