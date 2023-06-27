19aa5c0ca678de2a51e8059085a678ec87147c8f
guilhermevialle <vialleguilherme@gmail.com>
2023-06-27 02:13:48 -0300

    allowing cors

--- server/src/core.ts
+++ server/src/core.ts
@@ -9,7 +9,7 @@ const httpServer = http.createServer(expressServer)
 
 const io = new Server(httpServer, {
   cors: {
-    origin: 'http://localhost:3000',
+    origin: 'https://talkonline-app.vercel.app',
     credentials: true,
   },
 })
