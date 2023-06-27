085b71f858196acff4201554efe57fe478dc0b9f
guilhermevialle <vialleguilherme@gmail.com>
2023-06-27 02:53:31 -0300

    last one try

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -2,7 +2,7 @@ import { io } from 'socket.io-client'
 
 const server = {
   dev: 'http://localhost:9090',
-  prod: 'https://talkonline-app.vercel.app',
+  prod: 'https://talkonline-app-api.vercel.app',
 }
 
 export const socket = io(server.prod, {
