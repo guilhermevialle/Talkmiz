decbe9cae75dec36ea30bd83fc13b2dcbfa05237
guilhermevialle <vialleguilherme@gmail.com>
2023-06-27 03:18:26 -0300

    finally solve CORS problem because change vercel to Render HOST

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -2,9 +2,10 @@ import { io } from 'socket.io-client'
 
 const server = {
   dev: 'http://localhost:9090',
-  prod: 'https://talkonline-app-api.vercel.app',
+  vercel: 'https://talkonline-app-api.vercel.app',
+  render: 'https://latest-chat-api.onrender.com',
 }
 
-export const socket = io(server.prod, {
+export const socket = io(server.render, {
   withCredentials: true,
 })
