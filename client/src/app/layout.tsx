ebb620854a51191a456a5da1a269a008bfdaaa3c
guilhermevialle <vialleguilherme@gmail.com>
2023-06-27 04:42:04 -0300

    adding all favicons

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,5 +1,5 @@
 import './globals.css'
-import { Inter, Roboto, Poppins } from 'next/font/google'
+import { Poppins } from 'next/font/google'
 
 const poppins = Poppins({
   weight: ['100', '200', '300', '400', '500', '700', '800', '900'],
@@ -19,6 +19,81 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
+      <head>
+        <link
+          rel='apple-touch-icon'
+          sizes='57x57'
+          href='../../public/favicon_pack/apple-icon-57x57.png'
+        />
+        <link
+          rel='apple-touch-icon'
+          sizes='60x60'
+          href='../../public/favicon_pack/apple-icon-60x60.png'
+        />
+        <link
+          rel='apple-touch-icon'
+          sizes='72x72'
+          href='../../public/favicon_pack/apple-icon-72x72.png'
+        />
+        <link
+          rel='apple-touch-icon'
+          sizes='76x76'
+          href='../../public/favicon_pack/apple-icon-76x76.png'
+        />
+        <link
+          rel='apple-touch-icon'
+          sizes='114x114'
+          href='../../public/favicon_pack/apple-icon-114x114.png'
+        />
+        <link
+          rel='apple-touch-icon'
+          sizes='120x120'
+          href='../../public/favicon_pack/apple-icon-120x120.png'
+        />
+        <link
+          rel='apple-touch-icon'
+          sizes='144x144'
+          href='../../public/favicon_pack/apple-icon-144x144.png'
+        />
+        <link
+          rel='apple-touch-icon'
+          sizes='152x152'
+          href='../../public/favicon_pack/apple-icon-152x152.png'
+        />
+        <link
+          rel='apple-touch-icon'
+          sizes='180x180'
+          href='../../public/favicon_pack/apple-icon-180x180.png'
+        />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='192x192'
+          href='../../public/favicon_pack/android-icon-192x192.png'
+        />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='32x32'
+          href='../../public/favicon_pack/favicon-32x32.png'
+        />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='96x96'
+          href='../../public/favicon_pack/favicon-96x96.png'
+        />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='16x16'
+          href='../../public/favicon_pack/favicon-16x16.png'
+        />
+        <link rel='manifest' href='/manifest.json' />
+        <meta name='msapplication-TileColor' content='#ffffff' />
+        <meta name='msapplication-TileImage' content='/ms-icon-144x144.png' />
+        <meta name='theme-color' content='#ffffff'></meta>
+      </head>
       <body className={poppins.className + ' text-zinc-200'}>{children}</body>
     </html>
   )
